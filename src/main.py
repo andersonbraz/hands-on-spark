3c235243a5fb71a392378188fc2c16b4ee8b838c
Anderson Braz <contato@andersonbraz.com>
2023-02-06 23:07:38 -0300

    Up Files(s).

--- src/main.py
+++ src/main.py
@@ -1,29 +1,37 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
-from util.functions import drop_cols_duplicate
+from util.functions import drop_cols_duplicates, format_num_document
 
 spark = SparkSession.builder.appName("Hands on Spark").getOrCreate()
 
 
 if __name__ == "__main__":
+    schema_alunos = "Id INT, Nome STRING, Documento STRING"
 
-    schema_alunos = "Id INT, Nome STRING"
-
-    df_alunos = spark.read.csv(f"/Users/andersonbraz/Projects/hands-on-spark/src/data/alunos.csv",
-    header=True, schema=schema_alunos, sep=";");
+    df_alunos = spark.read.csv(
+        "/Users/andersonbraz/Projects/hands-on-spark/src/data/alunos.csv",
+        header=True,
+        schema=schema_alunos,
+        sep=";",
+    )
 
     df_alunos.printSchema()
     df_alunos.show()
 
     schema_notas = "Id INT, Nota STRING, Materia STRING"
 
-    df_notas = spark.read.csv(f"/Users/andersonbraz/Projects/hands-on-spark/src/data/notas.csv",
-    header=True, schema=schema_notas, sep=";");
+    df_notas = spark.read.csv(
+        "/Users/andersonbraz/Projects/hands-on-spark/src/data/notas.csv",
+        header=True,
+        schema=schema_notas,
+        sep=";",
+    )
 
     df_notas.printSchema()
     df_notas.show()
 
-    df_final = df_alunos.join(df_notas,df_alunos.Id == df_notas.Id, "inner")
-    df_final = drop_cols_duplicate(df_final)
+    df_final = df_alunos.join(df_notas, df_alunos.Id == df_notas.Id, "inner")
+    df_final = drop_cols_duplicates(df_final)
+    df_final = format_num_document("Documento", df_final)
 
     df_final.show()
--- src/util/functions.py
+++ src/util/functions.py
@@ -1,4 +1,8 @@
-def drop_cols_duplicate(df):
+from pyspark.sql import DataFrame
+from pyspark.sql import functions as f
+
+
+def drop_cols_duplicates(df):
     newcols = []
     dupcols = []
 
@@ -13,3 +17,12 @@ def drop_cols_duplicate(df):
         df = df.drop(str(dupcol))
 
     return df.toDF(*newcols)
+
+
+def format_num_document(field_name: str, df: DataFrame):
+    df = df.withColumn(
+        field_name,
+        f.lpad(f.regexp_replace(f.col(field_name), "[^a-zA-Z0-9]", ""), 11, "0"),
+    )
+
+    return df
