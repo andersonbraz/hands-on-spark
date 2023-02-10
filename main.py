1497f785a8f6d9a8286ff5d1f7c072a3404d71ca
Anderson Braz de Sousa <anderson.sousa@bancointer.com.br>
2023-02-10 11:29:49 -0300

    Up File(s).

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# from project.sample.minio.main import run_my_example
-from project.sample.join.main import run_my_example
-# from project.sample.enviroment.main import run_my_example
+# from project.samples.minio.main import run_my_example
+from project.samples.join.main import run_my_example
+# from project.samples.enviroment.main import run_my_example
 
 if __name__ == "__main__":
     run_my_example()
