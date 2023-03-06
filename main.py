15c1b4bc90f5686a6f83d374455cd52d09bb6cd8
Anderson Braz de Sousa <anderson.sousa@bancointer.com.br>
2023-03-06 13:14:14 -0300

    Up File(s).

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ TIMEZONE = "America/Sao_Paulo"
 
 def run():
     spark = SparkSession.builder.appName("Hands on Spark").getOrCreate()
-    schema_alunos = ("Id INT, Produto STRING, Plano STRING, Usuario STRING, DataAssinatura STRING, DataPagamento STRING, Status STRING")
+    schema_alunos = "Id INT, Produto STRING, Plano STRING, Usuario STRING," \
+        "DataAssinatura STRING, DataPagamento STRING, Status STRING"
 
     df_assinaturas = spark.read.csv(
         f"{PATH_PROJECT}/project/data/assinaturas.csv",
@@ -24,12 +25,10 @@ def run():
     
     df_assinaturas = format_local_datetime(["DataAssinatura", "DataPagamento"], df_assinaturas)
     df_assinaturas = df_assinaturas.withColumn("DiasExpirados", f.datediff(f.current_date(), f.from_utc_timestamp(f.col("DataAssinatura"), TIMEZONE)))
-    df_assinaturas = df_assinaturas.withColumn("Condition",
-                                               f.when(f.col("Status").isin(["FAILED","PENDING"]),"SIM")#.otherwise(f.lit(f.col("Status")))
-                                               )
-    df_assinaturas = df_assinaturas.withColumn("Condition",
-                                               f.when(f.col("Status").isin(["CANCELED",]),"TEST").otherwise(f.lit(f.col("Condition")))
-                                               )
+    df_assinaturas = df_assinaturas.withColumn("Condition", 
+                                               f.when(f.col("Status").isin(["FAILED", "PENDING"]),"SIM"))
+    df_assinaturas = df_assinaturas.withColumn("Condition", f.when(f.col("Status").isin(["CANCELED",]),"TEST").otherwise(f.lit(f.col("Condition"))))
+    
     df_assinaturas.printSchema()
     df_assinaturas.show()
 
